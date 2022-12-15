929f2f8a3933a3d576c0f6b44d10b28e1198bbd9
José Henrique Martins Pinheiro <pinheiro012345@gmail.com>
2022-12-15 18:01:38 -0300

    commit 4
    avanço nos conceitos de telas

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
 import PySimpleGUI as sg
+from random import randint
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 sg.theme('BlueMono')
 def telalogin():
@@ -21,7 +26,32 @@ def telalogin():
     telal.close()
 
 
-# telalogin()
+#telalogin()
+
+def telachave(key):
+    layout = [
+        [sg.Text("Digite a sua Chave", justification="c")],
+        [sg.Input(key="chave", size=(10,1)) ],
+        [sg.Text()],
+        [sg.Button("Voltar", s=6, button_color="red"),sg.Push(),sg.Button("Validar", size=(12,1)), sg.Push()],
+        
+    ]
+
+    telak = sg.Window(title="Validação", layout=layout, element_justification='c')
+
+    while True:
+        event, values = telak.read()
+        if event in (sg.WIN_CLOSED, "Voltar"): 
+            telak.disappear()
+        if event == "Validar":
+            if values["chave"] != key:
+                sg.PopupOK("Chave inválida")
+            else:
+                break
+
+    telak.close()
+
+
 
 def telacadastro():
     layout = [
@@ -43,16 +73,47 @@ def telacadastro():
 
     while True:
         event, values = telac.read()
-        if event == sg.WIN_CLOSED: 
+        if event == sg.WIN_CLOSED or event == "Sair": 
             break
-        
         if values["senha0"] != values["senha1"]:
-            sg.popup("Senhas Incoerentes...")    
-        
-        if values["senha0"] == values["senha1"]:
-            sg.popup("Senhas Compatíveis...")     
-        
+            sg.popup("Senhas Incoerentes...")              
+        if "@" not in values["email"] and ".com" not in values["email"]:
+            sg.popup("Email Inválido") 
+        if event == "Cadastrar":
+            for v in values:
+                if v == "":
+                    sg.PopupAutoClose(f"Campo {v} esta vazio!")
+                else:
+                    n = randint(000000, 999999)
+                    # validar usuario atraves do codigo por email 
+
+                    #   envio de código
+                    fromaddr = "atendimento@assesi.com"
+                    toaddr = values["email"]
+                    msg = MIMEMultipart()
+
+                    msg['From'] = fromaddr
+                    msg['To'] = toaddr
+                    msg['Subject'] = "aPonto"
+
+                    body = f"""
+                        <h2>Seja bem vindo {values["nome"].capitalize()}! ao novo sistema de ponto da Assesi</h2>
+                        <h3>sua chave é : {n}</h3>
+                        <p>Copie a sua chave e preencha no sistema para validar seu usuario!</p>
+                    """
+                    msg.attach(MIMEText(body, 'html'))
+                    #   iniciando o servidor
+                    server = smtplib.SMTP('smtp.gmail.com', 587)    # setando conexçao com o host do gmail
+                    server.starttls()   # iniciando a conexão
+                    server.login(fromaddr, "yplqyqrmkvhzpfnp")  # logando com o email que vai enviar o email
+                    text = msg.as_string()  # codificando as informações pro formato de leitura do gmail
+                    server.sendmail(fromaddr, toaddr, text)  # enviando as informações
+                    server.quit()   # fechando conexçao com o servidor
+                    #   termino de envio 
+        telac.disappear()
+        telachave(str(n))
     telac.close()
+    
 
 telacadastro()
 
