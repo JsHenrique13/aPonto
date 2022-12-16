4dd979540720304b3b066e59c538c8040d4f8b28
José Henrique Martins Pinheiro <pinheiro012345@gmail.com>
2022-12-16 17:30:40 -0300

    commit 5
    conceito de verificações

--- functions.py
+++ functions.py
@@ -6,12 +6,14 @@ from email.mime.text import MIMEText
 
 
 sg.theme('BlueMono')
+
+
 def telalogin():
     layout = [
-        [sg.Text("Email :", size=(10,1)),sg.Input(key="email", size=(25,1)) ],
-        [sg.Text("Senha :", size=(10,1)),sg.Input(key="senha", size=(25,1)) ],
+        [sg.Text("Email :", size=(10, 1)), sg.Input(key="email", size=(25, 1))],
+        [sg.Text("Senha :", size=(10, 1)), sg.Input(key="senha", size=(25, 1))],
         [sg.Text()],
-        [sg.Button("Sair"),sg.Push(),sg.Button("Login", size=(12,1)), sg.Push()],
+        [sg.Button("Sair"), sg.Push(), sg.Button("Login", size=(12, 1)), sg.Push()],
         
     ]
 
@@ -26,18 +28,18 @@ def telalogin():
     telal.close()
 
 
-#telalogin()
+    # telalogin()
 
 def telachave(key):
     layout = [
         [sg.Text("Digite a sua Chave", justification="c")],
-        [sg.Input(key="chave", size=(10,1)) ],
+        [sg.Input(key="chave", size=(10, 1))],
         [sg.Text()],
-        [sg.Button("Voltar", s=6, button_color="red"),sg.Push(),sg.Button("Validar", size=(12,1)), sg.Push()],
+        [sg.Button("Voltar", s=6, button_color="red"), sg.Push(), sg.Button("Validar", size=(12, 1)), sg.Push()],
         
     ]
 
-    telak = sg.Window(title="Validação", layout=layout, element_justification='c')
+    telak = sg.Window(title="Validação", layout=layout, element_justification='c', resizable=True, )
 
     while True:
         event, values = telak.read()
@@ -52,26 +54,26 @@ def telachave(key):
     telak.close()
 
 
-
 def telacadastro():
     layout = [
         [sg.Text()],
-        [sg.Text("Nome :", size=(12,1), justification="c"),sg.Input(key="nome", size=(25,1)) ],
-        [sg.Text("Email :", size=(12,1), justification="c"),sg.Input(key="email", size=(25,1)) ],
-        [sg.Text("Crie sua senha:",size=(12,1) ),sg.Input(key="senha0", size=(25,1), password_char="*")],
-        [sg.Text("Confirmar senha:",size=(12,1) ),sg.Input(key="senha1", size=(25,1), password_char="*")],
+        [sg.Text("Nome :", size=(12, 1), justification="c"), sg.Input(key="nome", size=(25, 1))],
+        [sg.Text("Email :", size=(12, 1), justification="c"), sg.Input(key="email", size=(25, 1))],
+        [sg.Text("Crie sua senha:", size=(12, 1)), sg.Input(key="senha0", size=(25, 1), password_char="*")],
+        [sg.Text("Confirmar senha:", size=(12, 1)), sg.Input(key="senha1", size=(25, 1), password_char="*")],
         [sg.Text()],
         [sg.Text("Selecione o seu respectivo Team: ")],
-        [sg.Push(),sg.Checkbox("Team Green", key="green"),sg.Push(),sg.Checkbox("Team Black", key="black"),sg.Push(),sg.Checkbox("Team Blue", key="blue"),sg.Push()],
+        [sg.Push(), sg.Checkbox("Team Green", key="green"), sg.Push(), sg.Checkbox("Team Black", key="black"), sg.Push(), sg.Checkbox("Team Blue", key="blue"), sg.Push()],
         [sg.Text()],
         [sg.Checkbox("É coordenador ?", key="coord")],
         [sg.Text()],
-        [sg.Button("Sair", s=6, button_color="tomato"),sg.Push(),sg.Button("Cadastrar", size=(12,1)), sg.Push()], 
+        [sg.Button("Sair", s=6, button_color="tomato"), sg.Push(), sg.Button("Cadastrar", size=(12, 1)), sg.Push()],
     ]
 
-    telac = sg.Window(title="Tela de Cadastro", layout=layout, element_justification='c', size=(400,350))
-
+    telac = sg.Window(title="Tela de Cadastro", layout=layout, element_justification='c', size=(400, 350))
+    send = 0
     while True:
+
         event, values = telac.read()
         if event == sg.WIN_CLOSED or event == "Sair": 
             break
@@ -81,43 +83,59 @@ def telacadastro():
             sg.popup("Email Inválido") 
         if event == "Cadastrar":
             for v in values:
-                if v == "":
-                    sg.PopupAutoClose(f"Campo {v} esta vazio!")
-                else:
-                    n = randint(000000, 999999)
-                    # validar usuario atraves do codigo por email 
+                while values[f"{v}"] == '':
+                    
+                    # print(f"campo {v} está vazio!")
+                    sg.popup_ok(f"Preencha o campo {v}!")
 
-                    #   envio de código
-                    fromaddr = "atendimento@assesi.com"
-                    toaddr = values["email"]
-                    msg = MIMEMultipart()
+        print(send)
+        if event == "Cadastrar" and send == 4:
+            try:
+                # sg.popup_ok("Email enviado!")
+                n = str(randint(000000, 999999))
+                # validar usuario atraves do codigo por email
 
-                    msg['From'] = fromaddr
-                    msg['To'] = toaddr
-                    msg['Subject'] = "aPonto"
+                #   envio de código
+                envio = "atendimento@assesi.com"
+                destino = values["email"]
+                msg = MIMEMultipart()
 
-                    body = f"""
-                        <h2>Seja bem vindo {values["nome"].capitalize()}! ao novo sistema de ponto da Assesi</h2>
+                msg['From'] = envio
+                msg['To'] = destino
+                msg['Subject'] = "aPonto"
+
+                body = f"""
+                        <h2>Seja bem vindo {values["nome"].capitalize()}! Ao novo sistema de ponto da Assesi</h2>
                         <h3>sua chave é : {n}</h3>
                         <p>Copie a sua chave e preencha no sistema para validar seu usuario!</p>
-                    """
-                    msg.attach(MIMEText(body, 'html'))
-                    #   iniciando o servidor
-                    server = smtplib.SMTP('smtp.gmail.com', 587)    # setando conexçao com o host do gmail
-                    server.starttls()   # iniciando a conexão
-                    server.login(fromaddr, "yplqyqrmkvhzpfnp")  # logando com o email que vai enviar o email
-                    text = msg.as_string()  # codificando as informações pro formato de leitura do gmail
-                    server.sendmail(fromaddr, toaddr, text)  # enviando as informações
-                    server.quit()   # fechando conexçao com o servidor
-                    #   termino de envio 
-        telac.disappear()
-        telachave(str(n))
-    telac.close()
-    
+        
+                            """
+                msg.attach(MIMEText(body, 'html'))
+                #   iniciando o servidor
+                server = smtplib.SMTP('smtp.gmail.com', 587)  # setando conexçao com o host do gmail
+                server.starttls()  # iniciando a conexão
+                server.login(envio, "yplqyqrmkvhzpfnp")  # logando com o email que vai enviar o email
+                text = msg.as_string()  # codificando as informações pro formato de leitura do gmail
+                server.sendmail(envio, destino, text)  # enviando as informações
+                server.quit()  # fechando conexçao com o servidor
+                #   termino de envio
+                sg.popup_ok("Email enviado!")
 
-telacadastro()
+                telachave(n)
+                print(values)
+                break
+
+            except:
+                continue
+
+    telac.close()
+
+
+# telacadastro()
+
 
 def telamain():
+
     layout = [
         [sg.Text()],
         [sg.Text("Nome :", size=(12,1), justification="c"),sg.Input(key="nome", size=(25,1)) ],
@@ -133,12 +151,53 @@ def telamain():
         [sg.Button("Sair"),sg.Push(),sg.Button("Cadastrar", size=(12,1)), sg.Push()], 
     ]
 
-    telac = sg.Window(title="Tela de Cadastro", layout=layout, element_justification='c', size=(800,450))
+    telam = sg.Window(title="Tela de Cadastro", layout=layout, element_justification='c', size=(800,450))
 
     while True:
-        event, values = telac.read()
+        event, values = telam.read()
         if event == sg.WIN_CLOSED: 
             break
         
         
-    telac.close()
+    telam.close()
+
+    """
+                    for v in values:
+                    if v == "":
+
+                        # sg.PopupAutoClose(f"Campo {v} esta vazio!")
+                    else:
+
+                        n = str(randint(000000, 999999))
+                        # validar usuario atraves do codigo por email 
+
+                        #   envio de código
+                        envio = "atendimento@assesi.com"
+                        destino = values["email"]
+                        msg = MIMEMultipart()
+
+                        msg['From'] = envio
+                        msg['To'] = destino
+                        msg['Subject'] = "aPonto"
+
+                        body = f"""
+    # <h2>Seja bem vindo {values["nome"].capitalize()}! ao novo sistema de ponto da Assesi</h2>
+    # <h3>sua chave é : {n}</h3>
+    # <p>Copie a sua chave e preencha no sistema para validar seu usuario!</p>
+
+    """
+    msg.attach(MIMEText(body, 'html'))
+    #   iniciando o servidor
+    server = smtplib.SMTP('smtp.gmail.com', 587)    # setando conexçao com o host do gmail
+    server.starttls()   # iniciando a conexão
+    server.login(envio, "yplqyqrmkvhzpfnp")  # logando com o email que vai enviar o email
+    text = msg.as_string()  # codificando as informações pro formato de leitura do gmail
+    server.sendmail(envio, destino, text)  # enviando as informações
+    server.quit()   # fechando conexçao com o servidor
+    #   termino de envio 
+
+    telachave(n)
+    break
+    """
+
+telacadastro()
